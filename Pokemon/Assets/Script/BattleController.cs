d7264474039fa29c2cb284f5e89c8875b91d266d
VictorFerreira69 <victor13.09.08ferreira@gmail.com>
2025-05-05 16:38:23 -0300

    Fazendo attacks

--- Pokemon/Assets/Script/BattleController.cs
+++ Pokemon/Assets/Script/BattleController.cs
@@ -1,11 +1,29 @@
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
-
+public enum Mode
+{
+    Phsical,Special,Status
+}
+public enum Type
+{
+    Normal,Fire,Water,Ghost
+}
+[System.Serializable]
+public class Attack
+{
+    [SerializeField] string name;
+    [SerializeField] Type type;
+    [SerializeField] Mode mode;
+    [SerializeField] int damage;
+    [SerializeField] int accuracy;
+    [SerializeField] int pp;
+}
 public class BattleController : MonoBehaviour
 {
     public static BattleController instance;
     public UnityEvent OnCharactersEnterEnd;
+   [SerializeField] Attack[] Attacks;
 
     private void Awake()
     {
